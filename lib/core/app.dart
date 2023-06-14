3cb1129afe58198bb33dca291b64287b532b2f8e
Shishir Rijal <ccrrizal0892@gmail.com>
2023-06-14 23:16:48 +0545

    added myapp in app

---
+++ lib/core/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp._internal(); // private constructor
+
+  // singleton instance
+  static MyApp instance = const MyApp._internal();
+
+  // This ensures MyApp is a singleton and and when accessed anywhere returns the same instance
+  factory MyApp() => instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
