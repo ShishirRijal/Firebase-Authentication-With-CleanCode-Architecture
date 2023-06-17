2128ba1dc7f7bc6fc7f9467a52947320e735df14
Shishir Rijal <ccrrizal0892@gmail.com>
2023-06-17 16:41:00 +0545

    color manager added

---
+++ lib/presentation/resources/color_manager.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color primary = Color(0XFF5A59D6);
+  static const Color accent = Color(0XFFDF5027);
+  static const Color black = Color(0XFF000000);
+  static const Color white = Color(0XFFFFFFFF);
+  static const Color error = Color(0XFFE61F34);
+  static const Color success = Colors.green;
+  static const Color grey = Color(0XFF9E9E9E);
+}
